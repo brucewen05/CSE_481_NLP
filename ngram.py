c116ca0130ef0ad86126cf72a9e832a99ea14003
Jianyang (Jane) Zhang <jianyz@uw.edu>
2017-04-30 07:20:22 -0700

    fix typo

--- ngram.py
+++ ngram.py
@@ -4,6 +4,7 @@ import operator
 import pickle
 from nltk import ngrams
 from collections import Counter
+from random import shuffle
 
 from sys import argv, stdin, exit
 
@@ -18,6 +19,7 @@ To load an existing model, do `python3 -l model_file`
 
 START = '<S>'
 END = '</S>'
+changyongpinyin = []
 
 # This function trains a model based on the
 # training file, and writes the training result:
@@ -32,7 +34,6 @@ def train(training_filename, output_filename):
     c = conn.cursor()
     rows = c.execute('SELECT p.characters, s.characters FROM pinyin_full_sentences as p JOIN full_sentences as s '
               'where p.sentence_id = s.sentence_id and p.file_id = s.file_id and p.text_id = s.text_id')
-
     unigrams = [START]
     bigrams = []
     for r in rows:
@@ -40,6 +41,7 @@ def train(training_filename, output_filename):
         bigrams.extend(ngrams(sequence=list(r[1]), n=2, left_pad_symbol=START, right_pad_symbol=END, pad_left=True, pad_right=True))
     bigram_dict = Counter(bigrams)
     unigram_dict = Counter(unigrams)
+    changyongpinyin = sorted(unigram_dict, key=lambda k: unigram_dict[k], reverse=True)[:9]
 
     rows = c.execute('SELECT p.char_num, p.character, w.characters as tup FROM words as w JOIN pinyin_characters as p '
               'where p.sentence_id = w.sentence_id and p.file_id = w.file_id and p.text_id = w.text_id and p.word_num = w.word_num')
@@ -69,21 +71,21 @@ def load_model(model_file):
 
 def predict(word_prev, pinyin_cur):
     unigram_dict, bigram_dict, dictionary = training_result
+    vocab_size = len(dictionary.keys())
     try:
         phrases = dictionary[pinyin_cur]
     except KeyError:
-        phrases = sorted(unigram_dict, key=lambda k: unigram_dict[k], reverse=True)[:9]
+        if len(changyongpinyin) != 0:
+            phrases = changyongpinyin
+        else:
+            phrases = ['我', '你', '的', '了']
 
     suggestion = {}
     for phrase in phrases:
         try:
-            suggestion[phrase] = (bigram_dict[word_prev, phrase] + 1) / float(unigram_dict[word_prev] + len(dictionary.keys()))
+            suggestion[phrase] = (bigram_dict[word_prev, phrase] + 1) / float(unigram_dict[word_prev] + vocab_size)
         except KeyError:
-            suggestion[phrase] = 1 / float(len(dictionary.keys()))
-
-    # For testing {prediction hanzi: score}
-    # print(suggestion)
-    # suggestion = sorted(suggestion, key=lambda k: suggestion[k], reverse=True)
+            suggestion[phrase] = 1 / float(vocab_size)
     return suggestion
 
 
--- pinyin_segment_eval.py
+++ pinyin_segment_eval.py
@@ -1,5 +1,42 @@
-import lcmc_queries
+import lcmc_queries as lcmc
+import pinyin_util
+import re
+import json
+from random import shuffle
 
-pinyin_sentences = lcmc_queries.get_pinyin_paragraphs()
-for s in pinyin_sentences:
-    print(s)
+
+def load_data2(filename):
+    try:
+        with open(filename, encoding='utf8') as f:
+            data = json.load(f)
+            return data
+    except IOError:
+        pass
+
+data = load_data2("data\sms_clean.data")[0]
+shuffle(data)
+with open("data/train_sms_clean.sample", 'w', encoding="utf-8") as trainoutfile:
+    train = json.dumps(list(data), indent=4, sort_keys=True)
+    trainoutfile.write(train)
+# with open("data/test_lcmc_clean.sample", 'w') as testoutfile:
+#     test = json.dumps(list(data[split_pos + 1:]), indent=4, sort_keys=True)
+#     testoutfile.write(test)
+
+# correct = total = 0
+# for tup in lcmc.get_pinyin_paragraphs():
+#     total += 1
+#     # drop tunes and add paddings
+#     p = re.sub(r'([a-z]+)[0-9] *', r' \1 ', tup[0])
+#     p = re.sub(r'([^a-z ])', '', p)
+#     # fix "uu"=>"v" in lcmc pinyin
+#     p = re.sub(r'uu', r'v', p)
+#     expected = p.split()
+#     segmented = pinyin_util.segment_with_hint(''.join(p.split()))
+#     if expected == segmented:
+#         correct += 1
+#     else:
+#         pass
+#
+# print(correct / float(total))
+
+
